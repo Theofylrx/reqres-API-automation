6ffdb8e7918b6008f5fe962fa953b8085883faf6
Leeks <bomvana.likho95@gmail.com>
2025-05-14 15:31:21 +0200

    Initial Setup
    - created a gradle kotlin project

---
+++ app/src/test/java/reqres/api/automation/AppTest.java
@@ -0,0 +1,11 @@
+package reqres.api.automation;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
